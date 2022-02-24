8e8ee8350d619e0242a0e39fdd52bda26a8cee28
Longstick0 <789456jang@naver.com>
2022-02-24 17:20:58 +0900

    initial commit

---
+++ src/main/java/FEB2022/core2/Core2Application.java
@@ -0,0 +1,12 @@
+package FEB2022.core2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core2Application.class, args);
+	}
+}
---
+++ src/main/java/FEB2022/core2/member/Grade.java
@@ -0,0 +1,4 @@
+package FEB2022.core2.member;
+
+public class Grade {
+}
